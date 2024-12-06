aa61c00325a7eed189c54b57f97a34626d9b780c
LongTran-Pionero <long.tran_it@pionero.io>
2024-12-06 12:00:55 +0700

    fix: follow feedback 12:00 241206

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,20 @@
 import './assets/main.css'
-import './assets/base.css'
+import './assets/output.css'
 
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
-import vuetify from '@/plugins/vuetify'
+
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
 import { VueQueryPlugin, QueryClient } from '@tanstack/vue-query'
 import router from './router'
 
 const app = createApp(App)
 const queryClient = new QueryClient()
+const vuetify = createVuetify()
 app.use(createPinia())
 app.use(vuetify)
 app.use(router)
