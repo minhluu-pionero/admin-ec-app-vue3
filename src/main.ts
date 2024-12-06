7b4280ff5a609e566beaae1e59194f4fe13ff4ca
LongTran-Pionero <long.tran_it@pionero.io>
2024-12-06 17:18:01 +0700

    [feat] setup lib (#10)
    * feat: setup lib
    
    * fix: follow feedback 12:00 241206
    
    * fix: base /admin-ec-app-vue3/ in git
    
    * feat: feedback 16:00 241206
    
    * chore/feedback 17:00 241206
    
    * chore: feedback 17:13 241206

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,17 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { createVuetify } from 'vuetify'
+import { VueQueryPlugin, QueryClient } from '@tanstack/vue-query'
 
-import App from './App.vue'
 import router from './router'
+import App from './App.vue'
 
 const app = createApp(App)
-
+const queryClient = new QueryClient()
+const vuetify = createVuetify()
 app.use(createPinia())
+app.use(vuetify)
 app.use(router)
-
+app.use(VueQueryPlugin, { queryClient })
 app.mount('#app')
