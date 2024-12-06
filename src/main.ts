854c9eb9d0b0c618ab99d127ae9c051ca17e7635
LongTran-Pionero <long.tran_it@pionero.io>
2024-12-06 10:16:04 +0700

    feat: setup lib

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,19 @@
 import './assets/main.css'
+import './assets/base.css'
+
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
+import vuetify from '@/plugins/vuetify'
+import { VueQueryPlugin, QueryClient } from '@tanstack/vue-query'
 import router from './router'
 
 const app = createApp(App)
-
+const queryClient = new QueryClient()
 app.use(createPinia())
+app.use(vuetify)
 app.use(router)
-
+app.use(VueQueryPlugin, { queryClient })
 app.mount('#app')
