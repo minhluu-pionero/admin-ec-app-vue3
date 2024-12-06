6a65f71088fbdaab81fee2ce7073719462a73252
LongTran-Pionero <long.tran_it@pionero.io>
2024-12-06 17:12:04 +0700

    chore/feedback 17:00 241206

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,9 @@
 import './assets/main.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import { createVuetify } from 'vuetify'
 import { VueQueryPlugin, QueryClient } from '@tanstack/vue-query'
-
 import router from './router'
-
 import App from './App.vue'
 
 const app = createApp(App)
