e05c6c30dee87c03b6149db2837e787bed8459fd
truonglongtran <longtran2308bn@gmail.com>
2024-12-10 13:44:14 +0700

    fix: feedback 13:44 241210

--- src/router/index.ts
+++ src/router/index.ts
@@ -20,12 +20,12 @@ const router = createRouter({
       component: () => import('@/layouts/AuthLayout.vue'),
       children: [
         {
-          path: ROUTES.login.name,
+          path: ROUTES.login.path,
           name: ROUTES.login.name,
           component: () => import('@/views/login-page/index.vue'),
         },
         {
-          path: ROUTES.forgotPassword.name,
+          path: ROUTES.forgotPassword.path,
           name: ROUTES.forgotPassword.name,
           component: () => import('@/views/forgot-password-page/index.vue'),
         },
