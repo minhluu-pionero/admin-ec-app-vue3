e0aab596ac13e4c767eacc42d66f5cdd88348a44
LongTran-Pionero <long.tran_it@pionero.io>
2024-12-11 10:53:35 +0700

    fix: feedback 10:53 241211

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,13 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import { ROUTES } from '@/utils/constants'
-import MainLayout from '@/layouts/main-layout/MainLayout.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      component: MainLayout,
+      component: () => import('@/layouts/main-layout/MainLayout.vue'),
       children: [
         {
           path: ROUTES.home.path,
