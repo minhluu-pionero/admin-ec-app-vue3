bbab39074d38f790db32639888b2e09508011ae0
LongTran-Pionero <long.tran_it@pionero.io>
2024-12-18 10:40:28 +0700

    fix: check user and token exist is loggedin

--- src/stores/authStore.ts
+++ src/stores/authStore.ts
@@ -3,15 +3,15 @@ import type { AuthState } from '@/types/auth.type'
 export const useAuthStore = defineStore('auth', {
   state: (): AuthState => ({
     token: 'hardcoded-token',
-    user: { email: 'user@example.com', name: 'Hardcoded User' },
+    //user: { email: 'user@example.com', name: 'Hardcoded User' },
     // token: null,
-    // user: null,
+    user: null,
     tokenExpiry: null,
     isTokenExpired: false,
   }),
 
   getters: {
-    isLoggedIn: (state) => !!state.token,
+    isLoggedIn: (state) => !!state.token && !!state.user,
   },
 
   actions: {
