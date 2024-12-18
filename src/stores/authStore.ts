34e47bccc830f8ac245de3bd75aa271c5193dc97
LongTran-Pionero <long.tran_it@pionero.io>
2024-12-18 10:36:43 +0700

    feat: use type state

--- src/stores/authStore.ts
+++ src/stores/authStore.ts
@@ -1,11 +1,11 @@
 import { defineStore } from 'pinia'
-
+import type { AuthState } from '@/types/auth.type'
 export const useAuthStore = defineStore('auth', {
-  state: () => ({
-    // token: 'hardcoded-token',
-    // user: { email: 'user@example.com', name: 'Hardcoded User' },
-    token: null,
-    user: null,
+  state: (): AuthState => ({
+    token: 'hardcoded-token',
+    user: { email: 'user@example.com', name: 'Hardcoded User' },
+    // token: null,
+    // user: null,
     tokenExpiry: null,
     isTokenExpired: false,
   }),
--- src/types/auth.type.ts
+++ src/types/auth.type.ts
@@ -6,3 +6,10 @@ export type UserType = {
   email: string
   name: string
 }
+
+export interface AuthState {
+  token: string | null
+  user: UserType | null
+  tokenExpiry: string | null
+  isTokenExpired: boolean
+}
