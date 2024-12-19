ae0de2e620197428e870f96315f9a4723a442344
truonglongtran <longtran2308bn@gmail.com>
2024-12-19 14:06:15 +0700

    fix: check code again 14:05 241219

--- src/middleware/middlewarePipeline.ts
+++ src/middleware/middlewarePipeline.ts
@@ -31,14 +31,9 @@ export default async function middlewarePipeline(
   }
 
   const nextWrapper: NavigationGuardNext = (param?: NextParamType) => {
-    if (param instanceof Error || typeof param === 'boolean' || param === undefined) {
-      return context.next(param);
-    }
-  
-    if (param) {
+    if (param instanceof Error || typeof param === 'boolean' || param === undefined || param) {
       return context.next(param as RouteLocationRaw);
     }
-  
     proceedNext();
   };
 
